b0bec67d1eb1132fec0196adaec2217fc2d8f5d2
Kristina Alabastro <kristinaa7@live.com>
2022-12-11 09:39:28 -0800

    added data using dayjs to javascript

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
+var dayDisplayEl = $('#currentDay');
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+ //Add coded to display the current date in the header of the page.
+$(function (displayDate) {
+  var rightNow = dayjs().format('dddd, MMMM DD, YYYY');
+  dayDisplayEl.text(rightNow);
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
