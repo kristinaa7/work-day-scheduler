09aeec987563b186075ac1d990326a5585e3265a
Kristina Alabastro <kristinaa7@live.com>
2022-12-13 00:07:41 -0800

    added time blocks and README file screenshot

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# Work Day Scheduler
+# Work Day Essential Scheduler App
 
-## Description 
+## Description
+This calendar planner application can help the user stay organized during the standard business hours 9am-5pm. Tasks can be added to each time block and has the funtionality to update the current date using the refresh button.
 
 ## Installation
 N/A
 
 ## Usage 
 
+
 ### Screenshot to Webpage 
-![Alt text](link)
+![Screenshot](screenshots/photo.png)
 
 ### Link to Deloyed Website 
 
--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,41 @@
-var dayDisplayEl = $('#currentDay');
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// allows jQuery to run, Wraps all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
+$(document).ready(function(){
 
- //Add coded to display the current date in the header of the page.
+//add coded to apply the past, present, or future class to each time block by comparing the id to the current hour.
+var rows = document.querySelectorAll(".row")
+for (let i = 0; i < rows.length; i++) {  
+  if (dayjs().hour() > rows[i].getAttribute("data-hour")) {
+    rows[i].classList.add("past")
+  }
+  else if (dayjs().hour() < rows[i].getAttribute("data-hour")){
+    rows[i].classList.add("future")
+  }
+  else {rows[i].classList.add("present")}
+}
+
+//setting a variable to get element by ID
+var dayDisplayEl = $('#currentDay');
+
+//setting a variable to create element
+var textarea = $('<textarea>')
+var containerEl = document.getElementsByClassName(".container-lg px-5")
+
+ //Add code to display the current date in the header of the page.
 $(function (displayDate) {
   var rightNow = dayjs().format('dddd, MMMM DD, YYYY');
   dayDisplayEl.text(rightNow);
+});
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+//this = line 105 in index
+//line 23 saves description to local storage, line 24 keeps text shown when saved
+$(".saveBtn").click(function(){
+  var value = $(this).siblings(".description").val()
+  var time = $(this).parent().attr("id")
+    localStorage.setItem(time,value)
+    localStorage.getItem(time,value)
+setTimeout(function(){
+  $(".notification").removeClass("hide")
+},3000) 
+ });
+
+})
